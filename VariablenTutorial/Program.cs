c2c6caae5fe4e3e4a53fdd6aa4efbe9371cc80c0
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:18:43 +0200

    Projektdateien hinzufügen.

---
+++ VariablenTutorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablenTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarieren
+            int alter;
+
+            //Initialisieren
+            alter = 21;
+
+            //Ausgeben
+            Console.WriteLine(alter);
+            alter = 18;
+            Console.WriteLine(alter);
+            Console.ReadKey();
+        }
+    }
+}
